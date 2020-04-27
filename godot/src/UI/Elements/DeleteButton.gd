7c925afc77b24bbf5144b571857adf84ff6787b1
Nathan Lovato <nathan@gdquest.com>
2020-04-27 11:29:45 -0600

    Redesign trash icon and button
    Closes #9

---
+++ godot/src/UI/Elements/DeleteButton.gd
@@ -0,0 +1,21 @@
+extends TextureButton
+
+
+const COLOR_HOVER := Color("#0d9ad1")
+const COLOR_PRESSED := Color("#1973dc")
+
+
+func _on_mouse_entered() -> void:
+	modulate = COLOR_HOVER
+
+
+func _on_mouse_exited() -> void:
+	modulate = Color.white
+
+
+func _on_button_down() -> void:
+	modulate = COLOR_PRESSED
+
+
+func _on_button_up() -> void:
+	modulate = COLOR_HOVER if is_hovered() else Color.white
