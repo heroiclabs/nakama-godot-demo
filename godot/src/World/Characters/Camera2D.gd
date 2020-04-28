4c7a85eb9a9d1bf2ea47a1e3b458e3ea6b87b0d9
Nathan Lovato <nathan@gdquest.com>
2020-04-27 20:44:38 -0600

    Address implicit float to int conversion warning

--- godot/src/World/Characters/Camera2D.gd
+++ godot/src/World/Characters/Camera2D.gd
@@ -2,8 +2,7 @@ extends Camera2D
 
 
 func set_limits(limits: Rect2) -> void:
-	limit_left = limits.position.x
-	limit_top = limits.position.y
-	limit_right = limits.position.x + limits.size.x
-	limit_bottom = limits.position.y + limits.size.y
-	
+	limit_left = int(limits.position.x)
+	limit_top = int(limits.position.y)
+	limit_right = int(limits.position.x + limits.size.x)
+	limit_bottom = int(limits.position.y + limits.size.y)
