cf31ea53347b3306ede97eb5d35538522a7c93cc
Nathan Lovato <nathan@gdquest.com>
2020-06-07 16:23:53 -0600

    Update tutorial world_control.lua

--- tutorial/nakama/modules/world_control.lua
+++ tutorial/nakama/modules/world_control.lua
@@ -13,13 +13,6 @@ function world_control.match_init(context, params)
     return state, tick_rate, label
 end
 
-function world_control.match_join_attempt(context, dispatcher, tick, state, presence, metadata)
-    if state.presences[presence.user_id] ~= nil then
-        return state, false, "User already logged in."
-    end
-    return state, true
-end
-
 function world_control.match_join(context, dispatcher, tick, state, presences)
     for _, presence in ipairs(presences) do
         state.presences[presence.user_id] = presence
@@ -40,6 +33,13 @@ end
 
 function world_control.match_terminate(context, dispatcher, tick, state, grace_seconds)
     return state
+end
+
+function world_control.match_join_attempt(context, dispatcher, tick, state, presence, metadata)
+    if state.presences ~= nil and state.presences[presence.user_id] ~= nil then
+        return state, false, "User already logged in."
+    end
+    return state, true
 end
 
 return world_control
